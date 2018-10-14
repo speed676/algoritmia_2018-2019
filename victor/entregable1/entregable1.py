c56253fa468e660f680aad665690f73cbd04bd4e
Victor <victor.vidasoft@gmail.com>
2018-10-14 21:12:03 +0200

    Lee del fichero y construye correctamente el laberinto

--- victor/entregable1/entregable1.py
+++ victor/entregable1/entregable1.py
@@ -4,69 +4,31 @@ from victor.lab3._aux.labyrinthviewer import LabyrinthViewer
 from random import shuffle
 import sys
 
-def create_labyrinth(rows, cols, n=0):
-    # for i in range(rows):
-    #     for j in range(cols):
-    #         vertices.append((i, j))
 
-    vertices = [(row, col) for row in range(rows)
-                for col in range(cols)]
+def load_labyrinth(fichero):
+    edges = set()
+    fila = 0
+    for linea in open(fichero, encoding='utf8'):
+        listaMuros = linea.split(',')
+        for col in range(len(listaMuros)):
+            muro = listaMuros[col]
+            if 'n' not in muro:
+                edges.add(((fila, col), (fila - 1, col)))
+            if 'e' not in muro:
+                edges.add(((fila, col), (fila, col + 1)))
+            if 's' not in muro:
+                edges.add(((fila, col), (fila + 1, col)))
+            if 'w' not in muro:
+                edges.add(((fila, col), (fila, col - 1)))
+        fila += 1
 
-    print(vertices)
-
-    mfs = MergeFindSet()
-
-    for elem in vertices:
-        mfs.add(elem)
-
-    edges = []
-    for row, col in vertices:
-        if row+1 < rows:
-            edges.append(((row, col), (row+1, col)))
-        if col+1 < cols:
-            edges.append(((row, col),(row, col+1)))
-
-    shuffle(edges)
-
-    contador = 0
-    corridors = []
-    for u, v in edges:
-
-        if mfs.find(u) != mfs.find(v):
-            mfs.merge(u, v)
-            corridors.append((u, v))
-        elif n > contador:
-            corridors.append((u, v))
-            contador+=1
-
-    return UndirectedGraph(E=corridors)
+    return UndirectedGraph(E=edges)
 
 
 if __name__ == '__main__':
-    alto = 100
-    ancho = 120
+    if len(sys.argv) == 2:
+        laberinto = load_labyrinth(sys.argv[1])
 
-    for argumento in sys.argv[1:]:
-        print(argumento)
+        lv = LabyrinthViewer(laberinto, canvas_width=600, canvas_height=400, margin=10)
 
-    archivo = open(argumento, 'r')
-    lineas = archivo.readlines()
-
-    linea = [i.strip() for i in lineas]
-    for i in linea:
-        item = i.split(',')
-        for j in item:
-            # print(j)
-            for k in j:
-                print(k)
-                # zona para saber que pared toca poner
-            print(",")
-            # zona para cambiar a un nuevo cuardante del laberinto
-
-    archivo.close  # cierra archivo
-
-    # laberinto = create_labyrinth(alto, ancho)
-    # # Obligatorio: Crea un LabyrinthViewer pasándole el grafo del laberinto
-    # lv = LabyrinthViewer(laberinto, canvas_width=800, canvas_height=600, margin=10)
-    #
-    # lv.run()
+        lv.run()
